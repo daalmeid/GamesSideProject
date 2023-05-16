75c1c8252b4a8e794745a8301542a8513c171e66
daalmeid <dariobarroca@gmail.com>
2023-05-16 23:50:16 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Breakout from "./games/breakout"
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <ul>
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li>
+				<Link to="/about">About</Link>
+			</li>
+		</ul>
+      </div>
+
+	  <Routes>
+			<Route path='/'>
+				<Route path='/' element={ <h1>Home</h1>} />
+				<Route path="about" element={ <Breakout />} />
+			</Route>
+		</Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/games/breakout/index.tsx
@@ -0,0 +1,9 @@
+import Board from "./Board"
+
+export default function Breakout() {
+  return (
+	<div>
+		<Board />
+	</div>
+  )
+}
